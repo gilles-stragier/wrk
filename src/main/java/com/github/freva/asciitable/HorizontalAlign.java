d777c0d4335f54bebb6e83b0c3fc5f5c38a04f30
Gilles Stragier <gilles.stragier@gmail.com>
2019-10-26 10:06:43 +0200

    AsciiTable it will be!

--- src/main/java/com/github/freva/asciitable/HorizontalAlign.java
+++
@@ -1,5 +0,0 @@
-package com.github.freva.asciitable;
-
-public enum HorizontalAlign {
-    LEFT, CENTER, RIGHT
-}
--- src/main/java/net/ocheyedan/wrk/output/CompactOutputter.java
+++ src/main/java/net/ocheyedan/wrk/output/CompactOutputter.java
@@ -3,15 +3,15 @@ package net.ocheyedan.wrk.output;
 import com.github.freva.asciitable.AsciiTable;
 import com.github.freva.asciitable.Column;
 import com.github.freva.asciitable.HorizontalAlign;
-import net.ocheyedan.wrk.cmd.trello.Cards;
 import net.ocheyedan.wrk.cmd.trello.TrelloId;
 import net.ocheyedan.wrk.ids.IdsAliasingManager;
 import net.ocheyedan.wrk.trello.Card;
-import net.ocheyedan.wrk.trello.TrelloObject;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static net.ocheyedan.wrk.trello.TrelloObject.Type.LIST;
 
 public class CompactOutputter extends DefaultOutputter {
 
@@ -22,9 +22,9 @@ public class CompactOutputter extends DefaultOutputter {
             new Column().header("id").with(c -> idsManager.findByTrelloId(c).get()),
             new Column().dataAlign(HorizontalAlign.LEFT).header("title").with(c -> c.getName()),
             new Column().dataAlign(HorizontalAlign.LEFT).header("labels").with(c -> c.getLabels().stream().map(l -> l.getName()).collect(Collectors.joining(","))),
-            new Column().header("list").with(c -> idsManager.findByTrelloId(new TrelloId(c.getIdList(), TrelloObject.Type.LIST)).orElse(c.getIdList()))
+            new Column().header("list").with(c -> idsManager.findByTrelloId(new TrelloId(c.getIdList(), LIST)).orElse(c.getIdList()))
         ));
 
-        System.out.println(table);
+        Output.print(table);
     }
 }
